57b709497ba04c156338ba8e5d294cb62fe552d8
Harshit <79516676+Harshxt@users.noreply.github.com>
2026-08-21 17:46:30 +0530

    feat: add DataInitializerConfig to seed default administrator user on startup

---
+++ src/main/java/com/litmus/authPortal/config/DataInitializerConfig.java
@@ -0,0 +1,47 @@
+package com.litmus.authPortal.config;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.litmus.authPortal.model.Users;
+import com.litmus.authPortal.model.enums.AuthProviderIdentity;
+import com.litmus.authPortal.repository.UsersRepository;
+
+@Configuration
+public class DataInitializerConfig {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UsersRepository usersRepository;
+
+    DataInitializerConfig(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
+        this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    CommandLineRunner initUsers(UsersRepository usersRepository, PasswordEncoder PasswordEncoder) {
+        return args -> {
+            String defaultEmail = "${SEED_EMAIL}";
+
+            if (usersRepository.findByEmail(defaultEmail) == null) {
+                Users admin = new Users();
+                admin.setUsername("heyllo");
+                admin.setEmail(defaultEmail);
+                admin.setPassword(passwordEncoder.encode("worlld"));
+                admin.setPhoneNumber(1239485594);
+                admin.setUserCreated(LocalDateTime.now());
+                admin.setLastModified(admin.getUserCreated());
+                admin.setAuthProvider(AuthProviderIdentity.LOCAL);
+
+                usersRepository.save(admin);
+                System.out.println(">>> Seeding default user done.");
+            }
+        };
+    }
+}
